2828597dd860ebb2af4efc1a097bfc40f1f98b8e
deniceForero <deniceforero@gmail.com>
2021-06-17 15:50:34 -0700

    point6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+/*
+ *task_6
+ *6-size.c
+ *
+ */
+#include <stdio.h>
+/**
+ * main - printf zize
+ * Return: 0
+ */
+int main(void)
+{
+printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of long int: %zu bytes\n", sizeof(long int));
+printf("Size of float: %zu bytes\n", sizeof(float));
+return (0);
+}
