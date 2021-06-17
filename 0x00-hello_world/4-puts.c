2440b7d67bcec087ce80360f9771fbdc6799bb29
deniceForero <deniceforero@gmail.com>
2021-06-17 14:08:35 -0700

    punto

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,7 @@
 
 int main(void)
 {
-/* Print whit function puts*/  
-puts("\"Programming is like building a multilingual puzzle");  
+/* Print whit function puts*/
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
-
