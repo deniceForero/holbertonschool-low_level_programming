aa008d9f60b5971e7a6e233338d648aa7aa743f2
deniceForero <deniceforero@gmail.com>
2021-06-17 13:45:22 -0700

    point 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
+/*
+print puts
+punto 4 0x00-hello_world
+*/
+
 #include <stdio.h>
 
 int main(void)
 {
   /* Print whit function puts*/
   
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
   
 return (0);
 }
