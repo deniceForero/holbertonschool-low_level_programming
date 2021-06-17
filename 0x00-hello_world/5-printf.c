bc996e0ad6acd427a0fa896bfbfb62c31917ec2b
deniceForero <deniceforero@gmail.com>
2021-06-17 15:10:25 -0700

    point5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
-/* 
- *task 5 
- *5-printf.c
+/*
+*task 5 
+*5-printf.c
  */
 #include <stdio.h>
 /**
  * main - printf with proper grammar
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
