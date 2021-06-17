e0f2217ad922d2d91dc7622f89076d664493a7ca
deniceForero <deniceforero@gmail.com>
2021-06-17 15:57:12 -0700

    punto_6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 printf("Size of char: %zu bytes\n", sizeof(char));
 printf("Size of int: %zu bytes\n", sizeof(int));
 printf("Size of long int: %zu bytes\n", sizeof(long int));
-printf("Size of long long: %zu bytes\n", sizeof(long long));
+printf("Size of long long int: %zu bytes\n", sizeof(long long));
 printf("Size of float: %zu bytes\n", sizeof(float));
 return (0);
 }
