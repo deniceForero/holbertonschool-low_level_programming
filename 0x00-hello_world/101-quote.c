f7890bb4caf22502e7cd68f2c1fad02a9dd30d8f
deniceForero <deniceforero@gmail.com>
2021-06-18 08:21:52 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/*
+ *print putchar
+ *punto 9 0x00-hello_world
+ */
+
+#include <stdio.h>
+/**
+ * main - Print whit function putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
