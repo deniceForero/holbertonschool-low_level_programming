6a41f7e011a6a5c7e06a7b2cb06ba4b9018d0248
deniceForero <deniceforero@gmail.com>
2021-06-19 16:08:12 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+/*
+ *Print the number
+ *9-print_commas
+*/
+#include <stdio.h>
+
+/**
+ * main -print The Last Digit
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+/*Declarar variables para num*/
+int num = 0;
+while (num <= 9)
+{
+putchar((num) +'0');
+num++;
+if (num == 10)
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
