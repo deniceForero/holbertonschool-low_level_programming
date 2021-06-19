084368b5298ccaa1ef951c1d4fd2481865860001
deniceForero <deniceforero@gmail.com>
2021-06-18 17:45:51 -0700

    7 point

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+/*
+*Print the last digit
+*7-print_tebahpla.c 
+*/
+
+#include <stdi.h>
+/**
+* main -print The Last Digit
+* return: 0
+*/
+int main(void)
+{
+int caracter = 122
+putchar (caracter);
+
+while (caracter >=97)
+{
+putchar (caracter);
+caracter--;
+}
+putchar("\n");
+return (0);
+}
