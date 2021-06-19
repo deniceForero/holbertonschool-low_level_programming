1269a65d31fa1c9d1f0166abb63e8c4e70b8ab77
deniceForero <deniceforero@gmail.com>
2021-06-19 09:36:17 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -13,21 +13,6 @@
 
 int main(void)
 {
-/*
-*Print the numbers  
-*6-print_numbers.c
-*/
-
-#include <stdio.h>
-
-/**
-  * main -print The Last Digit
-  *
-  * Return: 0
-*/
-
-int main(void)
-{
 /*declare variables for number*/
 int num = 9;
 while (num <= 0)
