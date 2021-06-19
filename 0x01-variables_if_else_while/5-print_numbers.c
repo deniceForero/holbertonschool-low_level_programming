a513e41e49bd409266791277734420601cba09db
deniceForero <deniceforero@gmail.com>
2021-06-19 07:52:14 -0700

    Point5numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -14,12 +14,13 @@
 int main(void)
 {
 /*Declarar variables para numero */
-int numero = 10;
+int numero = 9;
 int aux = 0;
 while (aux <= numero)
 {
-printf("%d\n", aux);
+printf("%d", aux);
 aux++;
 }
+printf("\n");
 return (0);
 }
