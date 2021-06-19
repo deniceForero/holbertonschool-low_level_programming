7570af3f3f136f46616ccfc9c751c3859836b317
deniceForero <deniceforero@gmail.com>
2021-06-19 15:03:45 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+/*
+ *Print Hexadecimal numbers
+ *8-print_hexa.c minúscula
+*/
+
+#include <stdio.h>
+
+/**
+ * main -print alphabet oper lower
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+/*Declarar variables para letter y num*/
+int num = 0;
+char letter = 97;
+while (num <= 9)
+{
+putchar((num) + '0');
+num++;
+}
+while (letter <= 102)
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return(0);
+}
