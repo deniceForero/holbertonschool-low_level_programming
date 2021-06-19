0bbd9160e161cbaeb1819f8526cd070f5e094758
deniceForero <deniceforero@gmail.com>
2021-06-18 22:03:27 -0700

    4Alfabeto

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+/*
+ *Print the Alphabet
+ *4-print_alphabet.c sin q y e
+ */
+
+#include <stdio.h>
+
+/**
+ * main -print alphabet oper lower
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+/*Declarar variables para a */
+int caracter = 97;
+while (caracter <= 122 && != 101 && !=113)
+{
+putchar(caracter);
+caracter++;
+}
+putchar('\n');
+return (0);
