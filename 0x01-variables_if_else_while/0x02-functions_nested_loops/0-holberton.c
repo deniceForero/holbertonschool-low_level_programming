d13582f08f62164f892b5250d19b042ae7525401
deniceForero <deniceforero@gmail.com>
2021-06-21 10:59:45 -0700

    0-holberton.c

---
+++ 0x01-variables_if_else_while/0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,27 @@
+/*
+ *Print the com
+ *10-print_comb two numbers
+*/
+#include <stdio.h>
+
+/**
+ * main -print comb two numbers
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+
+putchar('H');
+putchar('o');
+putchar('l');
+putchar('b');
+putchar('e');
+putchar('r');
+putchar('t');
+putchar('o');
+putchar('n');
+putchar('\n');
+return (0);
+}
