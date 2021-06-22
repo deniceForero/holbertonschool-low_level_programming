10de991736bb8739461e2f45172825efd381b2b5
deniceForero <deniceforero@gmail.com>
2021-06-22 09:00:06 -0700

    2print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,16 @@
 #include "holberton.h"
 
 /**
- * main -print alphabetx10
+ * print_alphabet_x10 - print 10 times
  *
- * Return: 0
+ * print for 10 times
 */
 
 void print_alphabet_x10(void)
 {
 int num = 0;
 char caracter;
-while(num++ <= 9)
+while (num++ <= 9)
 {
 for (caracter = 'a'; caracter <= 'z'; caracter++)
 _putchar(caracter);
