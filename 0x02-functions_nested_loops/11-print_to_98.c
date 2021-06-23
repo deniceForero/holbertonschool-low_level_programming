6d94c0cc848db16ecbbe728b9fb6d2f56e87bcab
deniceForero <deniceforero@gmail.com>
2021-06-23 11:22:13 -0700

    1111

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,34 +12,18 @@
 */
 void print_to_98(int n)
 {
-if (n < 98)
-{
-while (n <= 98)
-{
-printf("%d", n);
-if (n != 98)
-{
-printf(", ");
-}
-n++;
-}
-}
-else if (n > 98)
-{
-while (n >= 98)
-{
-printf("%d", n);
-if (n != 98)
-{
-printf(", ");
-}
-n--;
-}
-}
-else
-{
-printf("98");
-}
-printf("\n");
+  if (n >= 98)
+    {
+      while (n > 98)
+	printf("%d, ", n--);
+      printf("%d\n", n);
+    }
+
+  else
+    {
+      while (n < 98)
+	printf("%d, ", n++);
+      printf("%d\n", n);
+    }
 }
 
