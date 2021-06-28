76202d970edb9fb13b020d0ce4272b67a261a0c8
deniceForero <deniceforero@gmail.com>
2021-06-28 16:12:43 -0700

    0_pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+/*
+ *function reset
+ *0-reset_to_98.c 0x05-pointers_arrays_strings
+ */
+#include "holberton.h"
+
+/**
+ * reset_to_98 - *parameter and updates the value it points to to 98.
+ * 
+ * Return: value of 98
+ * 
+ * @n: value for pointer
+ */
+
+void reset_to_98(int *n)
+{
+  int *n;
+  *n = 98;
+}
+
