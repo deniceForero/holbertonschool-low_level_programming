263058bbe5717a56270d8ac4d62712f5b8c40fce
deniceForero <deniceforero@gmail.com>
2021-06-28 17:13:20 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+/*
+ *function swap
+ *1-swap.c 0x05-pointers_arrays_strings
+*/
+#include "holberton.h"
+
+/**
+ * swap_int - Write a function that swaps the values of two integers
+ * @*a: value for number one
+ * @*b: value for number two
+*/
+
+void swap_int(int *a, int *b)
+{
+int tem = a;
+*a = *b;
+*b = tem;
+}
+
